c593780c6bcb699f4414087a1ad05dd78e53dda4
himanshurtanikatech <ramanihimanshu58@gmail.com>
2023-11-07 11:04:35 +0530

    Updates

---
+++ precache-manifest.22d56703d2c6365705c52f12b7e77fa9.js
@@ -0,0 +1,390 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515a85b36354331ee08a4abb21d1eeb0",
+    "url": "/shreeji-frontEnd/index.html"
+  },
+  {
+    "revision": "232cf125d0d8a603f10a",
+    "url": "/shreeji-frontEnd/static/css/9.882fa26b.chunk.css"
+  },
+  {
+    "revision": "36e7fe8e3a601265be77",
+    "url": "/shreeji-frontEnd/static/css/main.2e77220b.chunk.css"
+  },
+  {
+    "revision": "1e16b1e757236991c39c",
+    "url": "/shreeji-frontEnd/static/js/0.422982cc.chunk.js"
+  },
+  {
+    "revision": "7e124b7e5a419635a31b",
+    "url": "/shreeji-frontEnd/static/js/1.6f5ed956.chunk.js"
+  },
+  {
+    "revision": "b0497ee3371f02971f9d",
+    "url": "/shreeji-frontEnd/static/js/10.a0440c23.chunk.js"
+  },
+  {
+    "revision": "19908d4d184282a18ca4",
+    "url": "/shreeji-frontEnd/static/js/11.71f3e9d3.chunk.js"
+  },
+  {
+    "revision": "e87af5f4147e776c01d3",
+    "url": "/shreeji-frontEnd/static/js/12.576b7d04.chunk.js"
+  },
+  {
+    "revision": "e3e3c528d610aa99df36",
+    "url": "/shreeji-frontEnd/static/js/13.a3b5dc48.chunk.js"
+  },
+  {
+    "revision": "da16596b283046669cc1",
+    "url": "/shreeji-frontEnd/static/js/14.2f06c720.chunk.js"
+  },
+  {
+    "revision": "406ad2b4bfb7fca4ac50",
+    "url": "/shreeji-frontEnd/static/js/15.cd8ff27a.chunk.js"
+  },
+  {
+    "revision": "cb2cda1109b1f22ae3fd",
+    "url": "/shreeji-frontEnd/static/js/16.847455a3.chunk.js"
+  },
+  {
+    "revision": "73196b0a37290073ca91",
+    "url": "/shreeji-frontEnd/static/js/17.2f26ea48.chunk.js"
+  },
+  {
+    "revision": "18c79fa9d900a6e1152d",
+    "url": "/shreeji-frontEnd/static/js/18.d9496033.chunk.js"
+  },
+  {
+    "revision": "5968c67ce23d8b5208be",
+    "url": "/shreeji-frontEnd/static/js/19.12beb5f2.chunk.js"
+  },
+  {
+    "revision": "4535c6c53a4d7a4b4589",
+    "url": "/shreeji-frontEnd/static/js/2.0f72fded.chunk.js"
+  },
+  {
+    "revision": "af11b9bc44b5f364b9f5",
+    "url": "/shreeji-frontEnd/static/js/20.6c75feea.chunk.js"
+  },
+  {
+    "revision": "f0e2de6012edf5e68417",
+    "url": "/shreeji-frontEnd/static/js/21.f029d6de.chunk.js"
+  },
+  {
+    "revision": "8e91ac87ad526932834f",
+    "url": "/shreeji-frontEnd/static/js/22.89b8e001.chunk.js"
+  },
+  {
+    "revision": "000201b9792ac73491df",
+    "url": "/shreeji-frontEnd/static/js/23.fd3058cf.chunk.js"
+  },
+  {
+    "revision": "88d989dc64cbb9a73990",
+    "url": "/shreeji-frontEnd/static/js/24.8be7d980.chunk.js"
+  },
+  {
+    "revision": "a1d320b8b92cf4e8fbc2",
+    "url": "/shreeji-frontEnd/static/js/25.3d5431c4.chunk.js"
+  },
+  {
+    "revision": "a721d64f8d9a4df92445",
+    "url": "/shreeji-frontEnd/static/js/26.f8ec02a5.chunk.js"
+  },
+  {
+    "revision": "e1c7a45035f023bb610d",
+    "url": "/shreeji-frontEnd/static/js/27.93d139c0.chunk.js"
+  },
+  {
+    "revision": "88cc23616c4f41660d78",
+    "url": "/shreeji-frontEnd/static/js/28.6ec4d902.chunk.js"
+  },
+  {
+    "revision": "25c76a5a2a381bb6bc8e",
+    "url": "/shreeji-frontEnd/static/js/29.42d3ed04.chunk.js"
+  },
+  {
+    "revision": "11d6292a277dda705525",
+    "url": "/shreeji-frontEnd/static/js/3.770c1b3f.chunk.js"
+  },
+  {
+    "revision": "bfde4c74ae1b79ded89d",
+    "url": "/shreeji-frontEnd/static/js/30.36ebf3d9.chunk.js"
+  },
+  {
+    "revision": "f653ad375ed302bfcf3b",
+    "url": "/shreeji-frontEnd/static/js/31.e643ecfd.chunk.js"
+  },
+  {
+    "revision": "9881a8696f8c36d2ad16",
+    "url": "/shreeji-frontEnd/static/js/32.d1a6c3e1.chunk.js"
+  },
+  {
+    "revision": "45c5a75a75c9cb765290",
+    "url": "/shreeji-frontEnd/static/js/33.0413acbf.chunk.js"
+  },
+  {
+    "revision": "467719f3bbf91821dced",
+    "url": "/shreeji-frontEnd/static/js/34.48d06860.chunk.js"
+  },
+  {
+    "revision": "53b693610f59e1c9f6cc",
+    "url": "/shreeji-frontEnd/static/js/35.995a5100.chunk.js"
+  },
+  {
+    "revision": "e748deab35642e58fac3",
+    "url": "/shreeji-frontEnd/static/js/36.7aaca47f.chunk.js"
+  },
+  {
+    "revision": "912f057a734c7a3d299a",
+    "url": "/shreeji-frontEnd/static/js/37.4a31113a.chunk.js"
+  },
+  {
+    "revision": "270634118ef687a15916",
+    "url": "/shreeji-frontEnd/static/js/38.eb47cbf7.chunk.js"
+  },
+  {
+    "revision": "220f8bbd0f4ee1025aa1",
+    "url": "/shreeji-frontEnd/static/js/39.d412fdee.chunk.js"
+  },
+  {
+    "revision": "a940c16e83743f5bcea8",
+    "url": "/shreeji-frontEnd/static/js/4.17305254.chunk.js"
+  },
+  {
+    "revision": "cc5329f7b3f2e0a2f0b5",
+    "url": "/shreeji-frontEnd/static/js/40.c40a4f39.chunk.js"
+  },
+  {
+    "revision": "43a8177315a64359c608",
+    "url": "/shreeji-frontEnd/static/js/41.63fe2bfa.chunk.js"
+  },
+  {
+    "revision": "2161a50a9832f3e7e2b8",
+    "url": "/shreeji-frontEnd/static/js/42.39ff5b7c.chunk.js"
+  },
+  {
+    "revision": "70a0346bf05da90edbb4",
+    "url": "/shreeji-frontEnd/static/js/43.5d31c597.chunk.js"
+  },
+  {
+    "revision": "cdb68156d9c4111f2df7",
+    "url": "/shreeji-frontEnd/static/js/44.93ed28b6.chunk.js"
+  },
+  {
+    "revision": "df3b0ea3abe9c118f486",
+    "url": "/shreeji-frontEnd/static/js/45.5ecb3a8c.chunk.js"
+  },
+  {
+    "revision": "9970c2a838d9ea0a8ac5",
+    "url": "/shreeji-frontEnd/static/js/46.8f9da9ec.chunk.js"
+  },
+  {
+    "revision": "05984ac8bdfea00dec89",
+    "url": "/shreeji-frontEnd/static/js/47.d864f05d.chunk.js"
+  },
+  {
+    "revision": "0c01db67bb7adb673ddb",
+    "url": "/shreeji-frontEnd/static/js/48.c116906a.chunk.js"
+  },
+  {
+    "revision": "6cffbb6cdb903c96a8ce",
+    "url": "/shreeji-frontEnd/static/js/49.2ae0bb13.chunk.js"
+  },
+  {
+    "revision": "67b0aa679d19510d7fc8",
+    "url": "/shreeji-frontEnd/static/js/5.bd5d2316.chunk.js"
+  },
+  {
+    "revision": "709eb2a71b4e7ef1f97a",
+    "url": "/shreeji-frontEnd/static/js/50.4765774d.chunk.js"
+  },
+  {
+    "revision": "d62dedb7e812a3744fa3",
+    "url": "/shreeji-frontEnd/static/js/51.a02c630c.chunk.js"
+  },
+  {
+    "revision": "93cf5b3fe9bb3528ac53",
+    "url": "/shreeji-frontEnd/static/js/52.7ee667ac.chunk.js"
+  },
+  {
+    "revision": "57c573364deab2bed122",
+    "url": "/shreeji-frontEnd/static/js/53.cec1796a.chunk.js"
+  },
+  {
+    "revision": "9d1ef35e4f6ac21f6ea1",
+    "url": "/shreeji-frontEnd/static/js/54.216a971f.chunk.js"
+  },
+  {
+    "revision": "d209b30f2df55defc49e",
+    "url": "/shreeji-frontEnd/static/js/55.40e940eb.chunk.js"
+  },
+  {
+    "revision": "86f6a1347e7794b6f949",
+    "url": "/shreeji-frontEnd/static/js/56.42e0eb80.chunk.js"
+  },
+  {
+    "revision": "5c990deb0f767fb9f7de",
+    "url": "/shreeji-frontEnd/static/js/57.286bc1c3.chunk.js"
+  },
+  {
+    "revision": "b6387efba05c8346c697",
+    "url": "/shreeji-frontEnd/static/js/58.c7e62357.chunk.js"
+  },
+  {
+    "revision": "e4dd25106e8a808a8eba",
+    "url": "/shreeji-frontEnd/static/js/59.c4fe44d1.chunk.js"
+  },
+  {
+    "revision": "d941248e4cbe0b729222",
+    "url": "/shreeji-frontEnd/static/js/6.16e61317.chunk.js"
+  },
+  {
+    "revision": "3360bdfb15feb40437f8",
+    "url": "/shreeji-frontEnd/static/js/60.9f7992fd.chunk.js"
+  },
+  {
+    "revision": "f970b9da7bd21659ca2c",
+    "url": "/shreeji-frontEnd/static/js/61.e48c89c9.chunk.js"
+  },
+  {
+    "revision": "f296ae506a7c7462d7be",
+    "url": "/shreeji-frontEnd/static/js/62.7b6d336b.chunk.js"
+  },
+  {
+    "revision": "e80922749b7f6bf43e0f",
+    "url": "/shreeji-frontEnd/static/js/63.405ba0c2.chunk.js"
+  },
+  {
+    "revision": "dd838647c16fffda492a",
+    "url": "/shreeji-frontEnd/static/js/64.c59a52fd.chunk.js"
+  },
+  {
+    "revision": "fee4be788fd1640333c6",
+    "url": "/shreeji-frontEnd/static/js/65.7b3fbba8.chunk.js"
+  },
+  {
+    "revision": "04cfd38c4da8d9bffe26",
+    "url": "/shreeji-frontEnd/static/js/66.82a0d283.chunk.js"
+  },
+  {
+    "revision": "a1fc4ac40195ea3c3707",
+    "url": "/shreeji-frontEnd/static/js/67.d79504bd.chunk.js"
+  },
+  {
+    "revision": "397b720e89aca403a9e4",
+    "url": "/shreeji-frontEnd/static/js/68.066db325.chunk.js"
+  },
+  {
+    "revision": "27d8023cf964586edf38",
+    "url": "/shreeji-frontEnd/static/js/69.8b0fa9ec.chunk.js"
+  },
+  {
+    "revision": "9db57a563bf56cc70354",
+    "url": "/shreeji-frontEnd/static/js/70.fb0d9458.chunk.js"
+  },
+  {
+    "revision": "292950f98ade1dd84e27",
+    "url": "/shreeji-frontEnd/static/js/71.3f0c4e16.chunk.js"
+  },
+  {
+    "revision": "0ba724c25bc4483d2a63",
+    "url": "/shreeji-frontEnd/static/js/72.c583d120.chunk.js"
+  },
+  {
+    "revision": "4a49256099e4b0405989",
+    "url": "/shreeji-frontEnd/static/js/73.e547f63d.chunk.js"
+  },
+  {
+    "revision": "dbe65c162cf38b4ae427",
+    "url": "/shreeji-frontEnd/static/js/74.1abbbdf1.chunk.js"
+  },
+  {
+    "revision": "f71562010082d1e380fa",
+    "url": "/shreeji-frontEnd/static/js/75.8a9d541c.chunk.js"
+  },
+  {
+    "revision": "ff0baf1394912ab44a3a",
+    "url": "/shreeji-frontEnd/static/js/76.efa30f4e.chunk.js"
+  },
+  {
+    "revision": "76ac6b8c35fb2fc25160",
+    "url": "/shreeji-frontEnd/static/js/77.cc670eea.chunk.js"
+  },
+  {
+    "revision": "76a055d862a800c1fe92",
+    "url": "/shreeji-frontEnd/static/js/78.cb5a8761.chunk.js"
+  },
+  {
+    "revision": "df02f199b3a22221cef0",
+    "url": "/shreeji-frontEnd/static/js/79.b6a7ea9a.chunk.js"
+  },
+  {
+    "revision": "168783abcd12c80890af",
+    "url": "/shreeji-frontEnd/static/js/80.3183a088.chunk.js"
+  },
+  {
+    "revision": "190f24310f1b5fafb911",
+    "url": "/shreeji-frontEnd/static/js/81.0eb485b7.chunk.js"
+  },
+  {
+    "revision": "232cf125d0d8a603f10a",
+    "url": "/shreeji-frontEnd/static/js/9.172152fb.chunk.js"
+  },
+  {
+    "revision": "36e7fe8e3a601265be77",
+    "url": "/shreeji-frontEnd/static/js/main.5dab97bc.chunk.js"
+  },
+  {
+    "revision": "c401766c1a69862a4aac",
+    "url": "/shreeji-frontEnd/static/js/runtime-main.408295a4.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/shreeji-frontEnd/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/shreeji-frontEnd/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/shreeji-frontEnd/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/shreeji-frontEnd/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/shreeji-frontEnd/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "65e8f7dd6a65aa5812afa9050bfc720e",
+    "url": "/shreeji-frontEnd/static/media/fontawesome-webfont.65e8f7dd.svg"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/shreeji-frontEnd/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/shreeji-frontEnd/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/shreeji-frontEnd/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/shreeji-frontEnd/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/shreeji-frontEnd/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/shreeji-frontEnd/static/media/lg.f4292655.ttf"
+  }
+]);
